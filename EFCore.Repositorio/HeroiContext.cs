c4262ebe48cbac8fa3d0b6ac2c953df1e6568887
Jailson Evangelista <jailson.teixeira@kobel.com.br>
2021-10-17 01:23:43 -0300

    Adequações no DbContext e Migrations

---
+++ EFCore.Repositorio/HeroiContext.cs
@@ -0,0 +1,32 @@
+﻿using EFCore.Dominio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.Repositorio
+{
+    public class HeroiContext : DbContext
+    {
+        public HeroiContext(DbContextOptions<HeroiContext> options) : base(options) { }
+        public DbSet<Heroi> Herois { get; set; }
+
+        public DbSet<Arma> Arma { get; set; }
+
+        public DbSet<Batalha> Batalha { get; set; }
+
+        public DbSet<HeroiBatalha> HeroiBatalha { get; set; }
+
+        public DbSet<IdentidadeSecreta> IdentidadeSecreta { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity =>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+            });
+        }
+
+    }
+}       
+       
--- EFCore.WebApi/Startup.cs
+++ EFCore.WebApi/Startup.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using EFCore.WebApi.Data;
+using EFCore.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +26,10 @@ namespace EFCore.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContext<HeroiContext>();
+            services.AddDbContext<HeroiContext>(options => 
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
